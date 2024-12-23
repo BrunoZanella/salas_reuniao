9d0ddf03b96c1710aaa179874b69039e2933ce2a
BrunoZanella <zanellabruno7@gmail.com>
2024-12-23 11:21:32 -0300

    publicando

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,8 +5,7 @@ from . import views
 urlpatterns = [
     path('cadastro/', views.cadastro, name='cadastro'),
     path('perfil/', views.perfil, name='perfil'),
-    path('login/', auth_views.LoginView.as_view(template_name='usuarios/login.html'), name='login'),
-#    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
 #    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('accounts/logout/', views.user_logout, name='logout'),
 ]
